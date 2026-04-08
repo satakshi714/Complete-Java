4f7af810fbaf0646d4560c5af445db0ed04d92db
Satakshi <sky.satakshi@gmail.com>
2026-04-08 09:54:36 +0530

    Added JavaScript files

---
+++ Frontend-Angular/JavaScript/JavaScriptNotes.js
@@ -0,0 +1,12 @@
+// Application of JS-
+// Multipage application
+// Single page application
+// Backend
+// Desktop Application
+// Game development
+// AI/ML
+// Mobile Application
+
+// Run time environment
+// Browser 
+// Non-Browser
