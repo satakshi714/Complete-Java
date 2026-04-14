0a41c49c5340b72db0ef7d97023dc3d4da89b17b
Satakshi <sky.satakshi@gmail.com>
2026-04-14 11:30:42 +0530

    Updated and added TS files

---
+++ Frontend-Angular/TypeScript/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let count = 20;
+let user = "Sam";
+let isActive = true;
+console.log(count, user, isActive);
---
+++ Frontend-Angular/TypeScript/script.ts
@@ -0,0 +1,5 @@
+let count: number = 20;
+let user: string = "Sam";
+let isActive: boolean = true;
+
+console.log(count, user, isActive);
